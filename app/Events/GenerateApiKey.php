b8595c59bb6495e0c5cdb3c971a27fd2799033e0
Damien Hebert <damien-1998@live.fr>
2023-10-01 14:28:59 +0200

    fix: GenerateApiKey

--- app/Events/GenerateApiKey.php
+++ app/Events/GenerateApiKey.php
@@ -2,11 +2,9 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
+use App\Models\User;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -17,8 +15,9 @@ class GenerateApiKey
     /**
      * Create a new event instance.
      */
-    public function __construct()
-    {
+    public function __construct(
+        public readonly User $user
+    ) {
         //
     }
 
